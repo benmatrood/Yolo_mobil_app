9a2de59c2b1bafb6435c32dd6c0f438df0500bba
kocedric <kocedric02@gmail.com>
2022-02-11 08:34:03 +0000

    Ajout de la doc

--- backend/yoyo-api/nlp.py
+++ backend/yoyo-api/nlp.py
@@ -1,20 +1,25 @@
-# Chargement des biblothèques
 import re
 import spacy
 
-# Charge le petit pipeline français
+# Charge le moyen pipeline français
 nlp = spacy.load("fr_core_news_md")
 
 
-#Fonction de tokenisation
 class Model:
     def __init__(self):
-        self.intension = None
-        self.montant = None
+        self.intent = None
+        self.amount = None
 
-    def cleanToken(self, sentences):
+    def cleanToken(self, sentences: str):
+        """
+        Applique un pipeline de nettoyage à la phrase
+        :param sentence:
+        :type sentences: str
+        :return: sentence
+        :rtype: str
+        """
         self.sentences = sentences
-        # Tokeniser la phrase en mettant les mots en miniscule
+        # Instanciation du pipeline sur la phrase préalablement mise en miniscule
         doc = nlp(sentences.lower())
 
         # On garde le token s'il ne fait pas partir des stop_words ni des signes
@@ -22,13 +27,13 @@ class Model:
         words = [token.lemma_ for token in doc if token.is_stop is not True and token.is_punct is not True]
         return ' '.join(words)
 
-    def checkKeyWord(self, sentence):
+    def checkKeyWord(self, sentence: str):
         """
-        Fonction qui récupère le mot clé et montant
+        Fonction qui extraire le mot clé et montant de la phrase de l'utilisateur
         :param sentence:
         :type sentence: str
         :return: intension
-        :return: montant
+        :return: amount
         :rtype: str
         """
 
@@ -38,26 +43,30 @@ class Model:
             'sms': ["message", "sms"]
         }
         try:
-            self.montant = re.findall('[0-9]+', sentence)[0]  # Récupère le montant
+            # On extrait le premier nombre contenu dans la phrase (il est considéré comme le montant)
+            self.amount = re.findall('[0-9]+', sentence)[0]
         except:
             pass
 
-        words = sentence.split()  # Variable qui découpe la phrase
+        words = sentence.split()  # Découpe la phrase d'entrée en mots
+
+        # On compare nos mots clés aux mots contenus dans la phrase
+        # lorsque nous trouvons une correspondance nous retournons le mot clé
         for key, word in key_words.items():
             for mot in word:
                 if mot in words:
-                    self.intension = key
-        return self.intension, self.montant
+                    self.intent = key
+        return self.intent, self.amount
 
 
 if __name__ == "__main__":
-        # Créé en traitant une chaine de caractères avec l'objet nlp
-        internet_sentence = "Je veux forfait de 500"
-        sms_sentence = "je veux souscrire à un message de 200"
-        cal_sentence = "souscription de pass appel de 300"
+    # Créé en traitant une chaine de caractères avec l'objet nlp
+    internet_sentence = "Je veux forfait de 500"
+    sms_sentence = "je veux souscrire à un message de 200"
+    cal_sentence = "souscription de pass appel de 300"
 
-        model = Model()
-        sentence = model.cleanToken(internet_sentence)
-        p, montant = model.checkKeyWord(sentence)
+    model = Model()
+    sentence = model.cleanToken(sms_sentence)
+    p, montant = model.checkKeyWord(sentence)
 
-        print(f"intension: {p}, montant:{montant}")
+    print(f"intension:{p}, montant:{montant}")
